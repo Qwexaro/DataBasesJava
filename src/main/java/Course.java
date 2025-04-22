427980a09501c23422d286771c3835620bb9e6d8
Qwexaro <Sergikstar25.gold@mail.ru>
2025-04-22 10:19:42 +0300

    Main

---
+++ src/main/java/Course.java
@@ -0,0 +1,86 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course {
+    private int id;
+    private String name;
+    private int duration;
+    private CourseType courseType;
+    private String description;
+    private int teacherId;
+    private int studentCount;
+    private double pricePerHour;
+    private List<Course> listAllCourse = new ArrayList<>();
+
+    public List<Course> getListAllCourse() {
+        return listAllCourse;
+    }
+
+    public void addCourse(List<Course> listAllCourse) {
+        this.listAllCourse = listAllCourse;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public CourseType getCourseType() {
+        return courseType;
+    }
+
+    public void setCourseType(CourseType courseType) {
+        this.courseType = courseType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public int getStudentCount() {
+        return studentCount;
+    }
+
+    public void setStudentCount(int studentCount) {
+        this.studentCount = studentCount;
+    }
+
+    public double getPricePerHour() {
+        return pricePerHour;
+    }
+
+    public void setPricePerHour(double pricePerHour) {
+        this.pricePerHour = pricePerHour;
+    }
+}
